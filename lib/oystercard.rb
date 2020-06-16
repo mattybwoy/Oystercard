d88fe4569a5985a1cdc28a947dfa7a375b2f85dd
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-16 16:56:25 +0000

    Step12 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,7 +6,6 @@ MINIMUM_FARE = 1
 
   def initialize
     @balance = 0
-    #@in_journey = false
   end
 
   def top_up(amount)
@@ -14,15 +13,8 @@ MINIMUM_FARE = 1
     @balance += amount
   end
 
-  #def deduct(amount)
-    #@balance -= amount
-  #end
-
   def touch_in(station)
     fail 'Insufficient credit' if @balance < MINIMUM_BALANCE
-    #if balance > 0
-    #@in_journey = true
-    #end
     @entry_station = station
   end
 
@@ -31,9 +23,7 @@ MINIMUM_FARE = 1
   end
 
   def touch_out
-    #@balance -= MINIMUM_FARE
     deduct(MINIMUM_FARE)
-    #@in_journey = false
     @entry_station = nil
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,11 +3,7 @@ require './lib/oystercard'
 describe Oystercard do
   subject(:oystercard) { Oystercard.new }
   let(:station) {double :station}
-  # describe 'initialize' do
-  #   it 'checks not in journey upon creation' do
-  #   expect(subject.in_journey?).to eq false
-  # end
-  #end
+  
   describe 'balance' do
     it 'returns 0 by default' do
       expect(subject.balance).to eq 0
@@ -23,21 +19,11 @@ describe Oystercard do
   expect{ subject.top_up 91 }.to raise_error "Balance exceeded"
   end
 
- #describe 'deduct' do
-    #it { is_expected.to respond_to(:deduct).with(1).argument }
-    #it 'deducts the amount' do
-    #expect { subject.deduct 3 }.to change { subject.balance }.by -3
-    #end
-  #end 
-  
-
   describe 'touch_in' do
     it { is_expected.to respond_to(:touch_in) }
     it 'allows you to touch in' do
       subject.top_up(1)
-      #expect(subject.balance).to_not be_zero
       expect(subject.touch_in(station)). to eq station
-     # expect(subject.in_journey?).to eq true
     end
     it 'raises an error if balance less than £1' do
       expect { subject.touch_in(station) }.to raise_error "Insufficient credit"
@@ -52,8 +38,6 @@ end
     it 'checks whether you are in journey' do
       subject.top_up(1)
       subject.touch_in(station)
-      #expect(subject.touch_in).to eq true
-      #expect(subject.in_journey?).to eq true
       expect(subject.in_journey?).to eq station
     end
   end
